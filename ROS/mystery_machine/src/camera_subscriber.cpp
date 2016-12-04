dd8af2ea336684b075abd44e7adfad93c48134e3
shanek21 <shanegkelly21@gmail.com>
2016-12-04 03:45:22 -0500

    Create bounded rectangles around cones seen in camera feed.

--- ROS/mystery_machine/src/camera_subscriber.cpp
+++ ROS/mystery_machine/src/camera_subscriber.cpp
@@ -1,184 +1,101 @@
-//Includes all the headers necessary to use the most common public pieces of the ROS system.
+#include <string>
+#include <stdlib.h>
+#include <vector>
 #include <ros/ros.h>
-//Use image_transport for publishing and subscribing to images in ROS
 #include <image_transport/image_transport.h>
-//Use cv_bridge to convert between ROS and OpenCV Image formats
 #include <cv_bridge/cv_bridge.h>
-//Include some useful constants for image encoding. Refer to: http://www.ros.org/doc/api/sensor_msgs/html/namespacesensor__msgs_1_1image__encodings.html for more info.
 #include <sensor_msgs/image_encodings.h>
-//Include headers for OpenCV Image processing
+//#include <opencv2/imgcodecs/imgcodecs.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-//Include headers for OpenCV GUI handling
+//#include <opencv2/imgproc_imgproc_c.h>
 #include <opencv2/highgui/highgui.hpp>
- 
-//Store all constants for image encodings in the enc namespace to be used later.
+
 namespace enc = sensor_msgs::image_encodings;
  
-//Declare a string with the name of the window that we will create using OpenCV where processed images will be displayed.
-static const char WINDOW[] = "Image Processed";
+static const std::string raw_window = "Raw";
+static const std::string filtered_window = "Filtered";
+static const std::string contoured_window = "Contoured";
  
-//Use method of ImageTransport to create image publisher
 image_transport::Publisher pub;
 
-int LowerH = 0;
-int LowerS = 0;
-int LowerV = 0;
-int UpperH = 180;
-int UpperS = 196;
-int UpperV = 170;
+int blur = 1;
+int lowerH = 0;
+int upperH = 23;
+int lowerS = 165;
+int upperS = 256;
+int lowerV = 203;
+int upperV = 256;
 
-void colorDetectionCallback(const sensor_msgs::ImageConstPtr& original_image)
+void callback(const sensor_msgs::ImageConstPtr& original_image)
 {
-    //Convert from the ROS image message to a CvImage suitable for working with OpenCV for processing
+    // Convert from ROS image message to OpenCV image
     cv_bridge::CvImagePtr cv_ptr;
     try
     {
-        //Always copy, returning a mutable CvImage
-        //OpenCV expects color images to use BGR channel order.
         cv_ptr = cv_bridge::toCvCopy(original_image, enc::BGR8);
     }
     catch (cv_bridge::Exception& e)
     {
-        //if there is an error during conversion, display it
-        ROS_ERROR("tutorialROSOpenCV::main.cpp::cv_bridge exception: %s", e.what());
+        ROS_ERROR("main.cpp::cv_bridge exception: %s", e.what());
         return;
     }
-	cv::Mat img_mask,img_hsv; 
-	cv::cvtColor(cv_ptr->image,img_hsv,CV_BGR2HSV);
-	cv::inRange(img_hsv,cv::Scalar(LowerH,LowerS,LowerV),cv::Scalar(UpperH,UpperS,UpperV),img_mask); 
-    //Display the image using OpenCV
-    cv::imshow(WINDOW, img_mask);
-    //Add some delay in miliseconds. The function only works if there is at least one HighGUI window created and the window is active. If there are several HighGUI windows, any of them can be active.
+
+    cv::medianBlur(cv_ptr->image, cv_ptr->image, blur*2+1);
+
+    // Apply HSV filter
+    cv::Mat img_mask, img_hsv;
+    std::vector<std::vector<cv::Point> > contours;
+    std::vector<cv::Vec4i> hierarchy;
+    cv::cvtColor(cv_ptr->image,img_hsv,CV_BGR2HSV);
+    cv::inRange(img_hsv,cv::Scalar(lowerH,lowerS,lowerV),cv::Scalar(upperH,upperS,upperV),img_mask);
+    cv::imshow(filtered_window, img_mask);
+    
+    // Find contours and create bounded rectangles
+    cv::findContours(img_mask, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+    std::vector<std::vector<cv::Point> > contours_poly( contours.size() );
+    std::vector<cv::Rect> boundRect( contours.size() );
+    std::vector<cv::Point2f>center( contours.size() );
+    std::vector<float>radius( contours.size() );
+    for( size_t i = 0; i < contours.size(); i++ )
+    {
+      cv::approxPolyDP(cv::Mat(contours[i]), contours_poly[i], 3, true);
+      boundRect[i] = cv::boundingRect(cv::Mat(contours_poly[i]));
+    }
+    for( size_t i = 0; i< contours.size(); i++ )
+    {
+      cv::Scalar color = cv::Scalar(255, 0, 0);
+      cv::rectangle(cv_ptr->image, boundRect[i].tl(), boundRect[i].br(), color, 1, 8, 0 );
+    }
+    cv::imshow(raw_window, cv_ptr->image);
+
     cv::waitKey(3);
-    /**
-    * The publish() function is how you send messages. The parameter
-    * is the message object. The type of this object must agree with the type
-    * given as a template parameter to the advertise<>() call, as was done
-    * in the constructor in main().
-    */
-    //Convert the CvImage to a ROS image message and publish it on the "camera/image_processed" topic.
+
+    // Convert from OpenCV image to ROS image message and publish
     pub.publish(cv_ptr->toImageMsg());
 }
-//This function is called everytime a new image is published
-void imageCallback(const sensor_msgs::ImageConstPtr& original_image)
-{
-    //Convert from the ROS image message to a CvImage suitable for working with OpenCV for processing
-    cv_bridge::CvImagePtr cv_ptr;
-    try
-    {
-        //Always copy, returning a mutable CvImage
-        //OpenCV expects color images to use BGR channel order.
-        cv_ptr = cv_bridge::toCvCopy(original_image, enc::BGR8);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-        //if there is an error during conversion, display it
-        ROS_ERROR("tutorialROSOpenCV::main.cpp::cv_bridge exception: %s", e.what());
-        return;
-    }
- 
-    //Invert Image
-    //Go through all the rows
-    for(int i=0; i<cv_ptr->image.rows; i++)
-    {
-        //Go through all the columns
-        for(int j=0; j<cv_ptr->image.cols; j++)
-        {
-            //Go through all the channels (b, g, r)
-            for(int k=0; k<cv_ptr->image.channels(); k++)
-            {
-                //Invert the image by subtracting image data from 255               
-                cv_ptr->image.data[i*cv_ptr->image.rows*4+j*3 + k] = 255-cv_ptr->image.data[i*cv_ptr->image.rows*4+j*3 + k];
-            }
-        }
-    }
-     
- 
-    //Display the image using OpenCV
-    cv::imshow(WINDOW, cv_ptr->image);
-    //Add some delay in miliseconds. The function only works if there is at least one HighGUI window created and the window is active. If there are several HighGUI windows, any of them can be active.
-    cv::waitKey(3);
-    /**
-    * The publish() function is how you send messages. The parameter
-    * is the message object. The type of this object must agree with the type
-    * given as a template parameter to the advertise<>() call, as was done
-    * in the constructor in main().
-    */
-    //Convert the CvImage to a ROS image message and publish it on the "camera/image_processed" topic.
-        pub.publish(cv_ptr->toImageMsg());
-}
- 
-/**
-* This tutorial demonstrates simple image conversion between ROS image message and OpenCV formats and image processing
-*/
+
 int main(int argc, char **argv)
 {
-    /**
-    * The ros::init() function needs to see argc and argv so that it can perform
-    * any ROS arguments and name remapping that were provided at the command line. For programmatic
-    * remappings you can use a different version of init() which takes remappings
-    * directly, but for most command-line programs, passing argc and argv is the easiest
-    * way to do it.  The third argument to init() is the name of the node. Node names must be unique in a running system.
-    * The name used here must be a base name, ie. it cannot have a / in it.
-    * You must call one of the versions of ros::init() before using any other
-    * part of the ROS system.
-    */
     ros::init(argc, argv, "image_processor");
-    /**
-    * NodeHandle is the main access point to communications with the ROS system.
-    * The first NodeHandle constructed will fully initialize this node, and the last
-    * NodeHandle destructed will close down the node.
-    */
+
     ros::NodeHandle nh;
-    //Create an ImageTransport instance, initializing it with our NodeHandle.
+
     image_transport::ImageTransport it(nh);
 
-	cv::namedWindow("Ball");
-	cv::createTrackbar("LowerH","Ball",&LowerH,180,NULL);
-	cv::createTrackbar("UpperH","Ball",&UpperH,180,NULL);
-	cv::createTrackbar("LowerS","Ball",&LowerS,256,NULL);
-	cv::createTrackbar("UpperS","Ball",&UpperS,256,NULL);
-	cv::createTrackbar("LowerV","Ball",&LowerV,256,NULL);
-	cv::createTrackbar("UpperV","Ball",&UpperV,256,NULL);
+    const std::string control_window = "Filter Controls";
 
-    //OpenCV HighGUI call to create a display window on start-up.
-    cv::namedWindow(WINDOW, CV_WINDOW_AUTOSIZE);
-    /**
-    * Subscribe to the "camera/image_raw" base topic. The actual ROS topic subscribed to depends on which transport is used. 
-    * In the default case, "raw" transport, the topic is in fact "camera/image_raw" with type sensor_msgs/Image. ROS will call 
-    * the "imageCallback" function whenever a new image arrives. The 2nd argument is the queue size.
-    * subscribe() returns an image_transport::Subscriber object, that you must hold on to until you want to unsubscribe. 
-    * When the Subscriber object is destructed, it will automatically unsubscribe from the "camera/image_raw" base topic.
-    */
-    //image_transport::Subscriber sub = it.subscribe("camera/image_raw", 1, imageCallback);
-    image_transport::Subscriber sub = it.subscribe("/image_raw", 1, colorDetectionCallback);
-    //OpenCV HighGUI call to destroy a display window on shut-down.
-    cv::destroyWindow(WINDOW);
-    /**
-    * The advertise() function is how you tell ROS that you want to
-    * publish on a given topic name. This invokes a call to the ROS
-    * master node, which keeps a registry of who is publishing and who
-    * is subscribing. After this advertise() call is made, the master
-    * node will notify anyone who is trying to subscribe to this topic name,
-    * and they will in turn negotiate a peer-to-peer connection with this
-    * node.  advertise() returns a Publisher object which allows you to
-    * publish messages on that topic through a call to publish().  Once
-    * all copies of the returned Publisher object are destroyed, the topic
-    * will be automatically unadvertised.
-    *
-    * The second parameter to advertise() is the size of the message queue
-    * used for publishing messages.  If messages are published more quickly
-    * than we can send them, the number here specifies how many messages to
-    * buffer up before throwing some away.
-    */
-    pub = it.advertise("camera/image_processed", 1);
-    /**
-    * In this application all user callbacks will be called from within the ros::spin() call. 
-    * ros::spin() will not return until the node has been shutdown, either through a call 
-    * to ros::shutdown() or a Ctrl-C.
-    */
+    cv::namedWindow(control_window);
+    cv::createTrackbar("blur", control_window, &blur, 10, NULL);
+    cv::createTrackbar("lowerH", control_window, &lowerH, 180, NULL);
+    cv::createTrackbar("upperH", control_window, &upperH, 180, NULL);
+    cv::createTrackbar("lowerS", control_window, &lowerS, 256, NULL);
+    cv::createTrackbar("upperS", control_window, &upperS, 256, NULL);
+    cv::createTrackbar("lowerV", control_window, &lowerV, 256, NULL);
+    cv::createTrackbar("upperV", control_window, &upperV, 256, NULL);
+
+    image_transport::Subscriber sub = it.subscribe("/image_raw", 1, callback);
+
+    //    cv::destroyWindow(main_window);
+
     ros::spin();
-    //ROS_INFO is the replacement for printf/cout.
-    ROS_INFO("tutorialROSOpenCV::main.cpp::No error.");
- 
 }
