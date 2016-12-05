d1e73cd98c9fc633a49320ec8ec11cc77947f5e3
kghite <kghite@gmail.com>
2016-12-05 16:25:51 +0000

    Working wall follower launch files

--- ROS/mystery_machine/scripts/lidar_following.sh
+++ ROS/mystery_machine/scripts/lidar_following.sh
@@ -2,7 +2,7 @@
 
 echo "Beginning LIDAR Following Mode"
 
-sudo chmod r+w /dev/ttyACM0
-sudo chmod r+W /dev/ttyACM1
+sudo chmod a+rw /dev/ttyACM0
+sudo chmod a+rw /dev/ttyACM1
 
 roslaunch mystery_machine lidar_following.launch
