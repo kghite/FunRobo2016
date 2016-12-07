cb96aae7ee137aac5039fce5bf4dac737be77316
kghite <kghite@gmail.com>
2016-12-07 16:00:42 -0500

    Race 4 waypoint follower

--- ROS/mystery_machine/scripts/arbiter.py
+++ ROS/mystery_machine/scripts/arbiter.py
@@ -60,10 +60,8 @@ class Midbrain_Arbiter(object):
 		#print(turn)
 		msg = Twist()
 		msg.linear.x = float(2*vel)/float((ARRAY_SIZE-1))-1
-		print(float(2*vel)/float((ARRAY_SIZE-1))-1)
 		msg.angular.z = float(2*turn)/float((ARRAY_SIZE-1))*0.25-0.25 #Turn a maximum of 0.25 instead of 1
-		print(float(2*turn)/float((ARRAY_SIZE-1))-1)
-		#print(msg)
+		print(msg)
 		self.cmd_vel_pub.publish(msg)
 
 if __name__ == '__main__':
